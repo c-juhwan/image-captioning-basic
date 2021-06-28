9fa20d5a3109aec338f6716f4bccc5881cbad6ff
Choi Juhwan <gold32317@gmail.com>
2021-06-28 14:51:35 +0900

    modified

--- run_all.sh
+++ run_all.sh
@@ -4,8 +4,6 @@ make
 python setup.py build
 python setup.py install
 cd ../../
-git clone https://github.com/yunjey/pytorch-tutorial.git
-cd pytorch-tutorial/tutorials/03-advanced/image_captioning/
 
 pip install -r requirements.txt
 chmod +x download.sh
